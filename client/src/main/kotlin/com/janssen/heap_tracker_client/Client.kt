2c153fc60309dcb90d575071084324de16ac8a2f
Arjan Janssen <arjan.janssen@gmail.com>
2025-11-15 17:59:37 +0100

    client now uses functional style

--- client/src/main/kotlin/com/janssen/heap_tracker_client/Client.kt
+++ client/src/main/kotlin/com/janssen/heap_tracker_client/Client.kt
@@ -1,9 +1,84 @@
 package com.janssen.heap_tracker_client
 
-import java.io.IOException
+import protos.Message
 import java.lang.Thread.sleep
+import java.net.ConnectException
 import java.net.InetSocketAddress
 import java.net.Socket
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+class Heap {
+    data class Alloc(val size: Long)
+
+    fun alloc(address: Long, size: Long) {
+        allocations[address] = Alloc(size)
+    }
+
+    fun free(address: Long) {
+        allocations.remove(address)
+    }
+
+    val allocations = mutableMapOf<Long, Alloc>()
+}
+class TrackedHeap(val heapOperations : List<HeapOperation>, val markers : List<Marker>) {
+    enum class HeapOperationType {
+        Alloc,
+        Free,
+    }
+    data class HeapOperation(val type: HeapOperationType, val durationSinceServerStart: Duration, val address: Long, val size: Long = 0)
+    data class Marker(val operationSequenceNumber: Long, val name: String)
+
+    fun print() {
+        println("Heap operations:")
+        heapOperations.forEach {
+            println("${it}")
+        }
+
+        println("Markers:")
+        markers.forEach {
+            println("${it}")
+        }
+    }
+
+    companion object {
+        fun concatenate(trackedHeaps: List<TrackedHeap>) : TrackedHeap {
+            val heapOperations = mutableListOf<HeapOperation>()
+            val markers = mutableListOf<Marker>()
+            trackedHeaps.forEach {
+                heapOperations.addAll(it.heapOperations);
+                markers.addAll(it.markers);
+            }
+            return TrackedHeap(heapOperations, markers)
+        }
+
+        fun fromProtobuf(message: protos.Message.HeapOperations) : TrackedHeap {
+            val validProtoHeapOperations = message.heapOperationList.filter {
+                it.type != Message.HeapOperations.HeapOperation.Type.UNRECOGNIZED
+            }
+            val heapOperations = validProtoHeapOperations.map {
+                    val durationSinceServerStart = it.microsSinceServerStart.toDuration(DurationUnit.MICROSECONDS);
+                    val heapOperationType =
+                        when (it.type) {
+                            Message.HeapOperations.HeapOperation.Type.TYPE_ALLOC ->
+                                HeapOperationType.Alloc
+                            else -> {
+                                HeapOperationType.Free
+                            }
+                        }
+                    TrackedHeap.HeapOperation(heapOperationType, durationSinceServerStart, it.address, it.size)
+                }
+
+            val markers = message.markerList.map {
+                Marker(it.lastOperationSequenceNumber, it.name);
+            }
+
+            return TrackedHeap(heapOperations, markers)
+        }
+    }
+
+}
 
 class Client {
     constructor() {
@@ -12,35 +87,36 @@ class Client {
         try {
             socket.connect(socketAddress)
         }
-        catch (e: IOException) {
+        catch (e: ConnectException) {
+            println("Unable to connect to heap-tracker server at address $socketAddress: ${e.message}")
+        }
+        catch (e: Exception) {
             println("Exception: ${e.message}")
             e.printStackTrace()
             return
         }
 
-        var inputStream = socket.inputStream
-        while (true) {
-            sleep(500)
-            println("Checking bytes")
-            val bytesAvailable = inputStream.available()
-            if (bytesAvailable == 0) {
-                continue
+        val trackedHeaps = mutableListOf<TrackedHeap>()
+        while (!socket.isClosed) {
+            sleep(100)
+            val bytesAvailable = socket.inputStream.available()
+            if (bytesAvailable >  0) {
+                val bytesSent = socket.inputStream.readNBytes(bytesAvailable)
+                val message = protos.Message.HeapOperations.parseFrom(bytesSent)
+                trackedHeaps.add(TrackedHeap.fromProtobuf(message))
+                if (message.endOfFile) {
+                    println("End of file. Closing connection");
+                    socket.close();
+                }
             }
-
-            val bytesSent = inputStream.readNBytes(bytesAvailable)
-            val heapOperations = protos.Message.HeapOperations.parseFrom(bytesSent)
-
-            heapOperations.heapOperationList.forEach {
-                println("Type: ${it.type}")
-                println("Offset: ${it.address}")
-                println("Size: ${it.size}")
-            }
-
-            println("Bytes received ${bytesSent.size} bytes!");
         }
+
+        val trackedHeap = TrackedHeap.concatenate(trackedHeaps);
+        trackedHeap.print()
     }
 }
 
 fun main() {
+    println("Heap tracker (c) 2025 by Arjan Janssen")
     var client = Client()
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,28 @@
 use protobuf::Message;
 use std::{
     alloc::{GlobalAlloc, Layout, System},
+    time::SystemTime,
     cell::Cell,
     io::prelude::*,
     net::{TcpListener, TcpStream},
-    sync::{mpsc::{self, SyncSender, Receiver}, atomic::{AtomicPtr, Ordering}},
-    thread, thread::JoinHandle,
+    sync::{mpsc::{self, SyncSender, Receiver, channel}, atomic::{AtomicPtr, Ordering}},
+    thread, thread::ThreadId, thread::JoinHandle,
     time::Duration,
 };
 mod generated;
 
 #[derive(Debug)]
-pub enum HeapOperation {
-    Alloc {
-        address: usize,
-        layout: Layout,
-        thread_id: thread::ThreadId,
-    },
-    Dealloc {
-        address: usize,
-        layout: Layout,
-        thread_id: thread::ThreadId,
-    },
+pub enum HeapOperationKind {
+    Alloc,
+    Dealloc,
+}
+
+#[derive(Debug)]
+pub struct HeapOperation {
+    address: usize,
+    layout: Layout,
+    thread_id: ThreadId,
+    kind: HeapOperationKind,
 }
 
 pub struct Server {
@@ -32,32 +33,59 @@ pub struct Server {
     sender: SyncSender<HeapOperation>,
     receiver: Receiver<HeapOperation>,
     num_heap_operations_sent: usize,
+    start_time: SystemTime,
 }
 
 pub enum NetworkError {
     ConnectionError,
 }
 
+impl Drop for Server {
+    fn drop(&mut self) {
+        self.flush(true);
+        self.stream.shutdown(std::net::Shutdown::Both).expect("Unable to shutdown TCP communication stream");
+        println!("Num heap operations sent: {}", self.num_heap_operations_sent);
+        println!("Closing server...");
+    }
+}
+
 impl Server {
     pub fn new(
         max_heap_operations: usize,
     ) -> Result<Server, NetworkError> {
-        let (mut sender, receiver) = mpsc::sync_channel::<HeapOperation>(1 as usize);
+        let (sender, receiver) = mpsc::sync_channel::<HeapOperation>(1 as usize);
 
         let mut proto = generated::message::HeapOperations::new();
         proto.heap_operations.reserve(max_heap_operations);
 
-        let server_thread_id = thread::current().id();
         match Self::establish_connection() {
-            Ok(stream) => Ok(Self {
-                max_heap_operations,
-                proto,
-                stream,
-                server_thread_id,
-                sender,
-                receiver,
-            }),
+            Ok(stream) => {
+                let server_thread_id = thread::current().id();
+                let server_start_time = SystemTime::now();
+            
+                Ok(Self {
+                    max_heap_operations,
+                    proto,
+                    stream,
+                    server_thread_id,
+                    sender,
+                    receiver,
+                    num_heap_operations_sent: 0,
+                    start_time: server_start_time,
+                })
+            },
             Err(e) => Err(e),
+        }
+    }
+
+    pub fn send(&mut self, heap_op : HeapOperation) {
+        if thread::current().id() == self.server_thread_id {
+            return;
+        }
+
+        let result = self.sender.send(heap_op);
+        if result.is_err() {
+            println!("Send error");
         }
     }
 
@@ -69,103 +97,83 @@ impl Server {
         Err(NetworkError::ConnectionError)
     }
 
-    fn flush_if_needed(&mut self) {
-        //if self.proto.heap_operations.iter().count() >= self.max_heap_operations {
-        self.flush();
-        //}
-    }
-
-    pub fn flush(&mut self) {
-        let mut protoBytesBuffer: Vec<u8> = vec![];
-        let _ = self.proto.write_to_vec(&mut protoBytesBuffer);
-        let result = self.stream.write(&protoBytesBuffer);
+    fn flush(&mut self, end_of_file: bool) {
+        let mut proto_bytes_buffer: Vec<u8> = vec![];
+        self.proto.end_of_file = end_of_file;
+        let _ = self.proto.write_to_vec(&mut proto_bytes_buffer);
+        let result = self.stream.write(&proto_bytes_buffer);
         if result.is_err() {
             println!("Unable to write heap operations to socket stream! Connection lost?");
         }
-
+        
         self.num_heap_operations_sent += self.proto.heap_operations.len();
         self.proto.clear();
+        self.stream.flush().expect("Unable to flush heap operation TCP stream");
     }
 
-    fn send(&mut self, heap_op : HeapOperation) {
-        if thread::current().id() == self.server_thread_id {
-            return;
-        }
+    fn create_proto_heap_op(duration_since_server_start: Duration, heap_op: HeapOperation) -> generated::message::HeapOperation {
+        let mut proto_op = generated::message::HeapOperation::new();                
+        proto_op.micros_since_server_start = duration_since_server_start.as_micros().try_into().unwrap();
+        proto_op.address = heap_op.address as i64;
+        proto_op.size = Some(heap_op.layout.size() as i64);
+        proto_op.thread_id = 0;
+        proto_op.type_ = ::protobuf::EnumOrUnknown::new(
+            match heap_op.kind {
+                HeapOperationKind::Alloc => generated::message::heap_operation::Type::TYPE_ALLOC,
+                HeapOperationKind::Dealloc => generated::message::heap_operation::Type::TYPE_FREE,
+            });
 
-        let result = self.sender.send(heap_op);
-        if result.is_err() {
-            println!("Send error");
-        }
+        proto_op
     }
 
     fn push(&mut self, heap_op: HeapOperation) {
-        match heap_op {
-            HeapOperation::Alloc {
-                address,
-                layout,
-                thread_id,
-            } => {
-                let mut proto_op = generated::message::HeapOperation::new();
-                proto_op.type_ = ::protobuf::EnumOrUnknown::new(
-                    generated::message::heap_operation::Type::TYPE_ALLOC,
-                );
-                proto_op.address = address as i64;
-                proto_op.size = Some(layout.size() as i64);
-                proto_op.thread_id = 0;
-                self.proto.heap_operations.push(proto_op);
-            }
-            HeapOperation::Dealloc {
-                address,
-                layout,
-                thread_id,
-            } => {
-                let mut proto_op = generated::message::HeapOperation::new();
-                proto_op.type_ = ::protobuf::EnumOrUnknown::new(
-                    generated::message::heap_operation::Type::TYPE_FREE,
-                );
-                proto_op.address = address as i64;
-                proto_op.size = Some(layout.size() as i64);
-                proto_op.thread_id = 0;
-                self.proto.heap_operations.push(proto_op);
-            }
-        }
+        let duration_since_server_start = SystemTime::now().duration_since(self.start_time).expect("Unable to calculate delta time");
+        self.proto.heap_operations.push(Self::create_proto_heap_op(duration_since_server_start, heap_op));
 
-        self.flush_if_needed();
+        if self.proto.heap_operations.iter().count() >= self.max_heap_operations {
+            self.flush(false);
+        }
     }
 
-    fn run(&mut self) {
-        loop {
-            match self.receiver.recv() {
+    fn run(&mut self, terminate_receiver: Receiver<()>) {
+        while terminate_receiver.try_recv().is_err() {
+            match self.receiver.recv_timeout(Duration::from_millis(50)) {
                 Ok(heap_op) => {
                     self.push(heap_op);
                 }
-
-                Err(e) => {
-                    println!("Recv Error!!!")
+                Err(_) => {
                 }
             }
         }
     }
 }
 
-pub fn run_server() -> Result<JoinHandle<()>, std::io::Error> {
-    thread::Builder::new()
+pub fn run_server(terminate_receiver: Receiver<()>) -> Result<JoinHandle<()>, std::io::Error> {
+    let (connection_sender,connection_receiver) = channel();
+
+    let server_thread_id = thread::Builder::new()
         .name("heap-tracker".to_string())
         .spawn(move || {
-            println!("Running server");
-            let mut server = Server::new(2);
-    
+            println!("Server thread started!");
+            let server = Server::new(10);
             match server {
                 Ok(mut server) => {
                     SERVER.store(&mut server, Ordering::Release);
-                    println!("Connection established!");
-                    server.run();
+                    connection_sender.send(()).expect("Could not send connection established message");
+                    server.run(terminate_receiver);
+                    SERVER.store(std::ptr::null_mut(), Ordering::Release);
                 }
                 Err(_) => {
                     println!("Connection error!");
                 }
             }
-        })
+        });
+
+    println!("Waiting for connection...");
+    connection_receiver.recv().expect("Could not receive connection established message");
+    println!("Connection established!");
+
+    server_thread_id
 }
 
 static SERVER: AtomicPtr<Server> =
@@ -195,50 +203,50 @@ impl ScopedRecursionDepthLimiter {
 
 impl Drop for ScopedRecursionDepthLimiter {
     fn drop(&mut self) {
-        RECURSION_DEPTH.with(|depth| depth.set(depth.get() - 1));
-        RECURSION_DEPTH.with(|depth| assert!(depth.get() >= 0));
+        RECURSION_DEPTH.with(|depth| {
+            depth.set(depth.get() - 1);
+            assert!(depth.get() >= 0);
+        });
     }
 }
 
 unsafe impl GlobalAlloc for TrackedAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let ptr = unsafe { System.alloc(layout) };
-        let server_ptr = SERVER.load(Ordering::Acquire);
-        if !server_ptr.is_null() {
-            let recursion_limiter = ScopedRecursionDepthLimiter::new(2);
-            if recursion_limiter.limit_reached() {
-                return ptr;
-            }
-
-            unsafe {
-                (*server_ptr).send(HeapOperation::Alloc {
-                    address: ptr as usize,
-                    layout: layout,
-                    thread_id: thread::current().id(),
-                });
-            }
+        unsafe {
+            self.hook(HeapOperationKind::Alloc, layout, ptr);
         }
-
         ptr
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe {
+            self.hook(HeapOperationKind::Dealloc, layout, ptr);
+        }
+        unsafe { System.dealloc(ptr, layout) }
+    }    
+}
+
+impl TrackedAllocator {
+    unsafe fn hook(&self, kind: HeapOperationKind, layout: Layout, ptr: *mut u8) -> () {
         let server_ptr = SERVER.load(Ordering::Acquire);
-        if !server_ptr.is_null() {
-            let recursion_limiter = ScopedRecursionDepthLimiter::new(2);
-            if recursion_limiter.limit_reached() {
-                return;
-            }
-            unsafe {
-                (*server_ptr).send(HeapOperation::Dealloc {
-                    address: ptr as usize,
-                    layout: layout,
-                    thread_id: thread::current().id(),
-                });
-            }            
+        if server_ptr.is_null() {
+            return
         }
 
-        unsafe { System.dealloc(ptr, layout) }
+        let recursion_limiter = ScopedRecursionDepthLimiter::new(2);
+        if recursion_limiter.limit_reached() {
+            return;
+        }
+
+        unsafe {
+            (*server_ptr).send(HeapOperation{
+                address: ptr as usize,
+                layout: layout,
+                thread_id: thread::current().id(),
+                kind: kind,
+            });
+        }
     }
 }
 
@@ -248,14 +256,16 @@ static mut GLOBAL_ALLOCATOR: TrackedAllocator = TrackedAllocator {};
 fn main() {
     println!("Heap tracker by Arjan Janssen");
 
-    let server_thread = run_server();
+    let (terminate_sender, terminate_receiver) = channel();
+    let server_thread = run_server(terminate_receiver);
 
-    let mut v = vec![1, 2, 3];
-    while true {
-        thread::sleep(Duration::from_millis(500));
-        println!("Increase vector capacity");
-        v.reserve(v.capacity() + 10);
+    let mut growing_vec = vec![1, 2, 3];
+    for i in 1..10000 {
+        growing_vec.push(i);
     }
+
+    println!("Sending terminate signal to server thread!");
+    terminate_sender.send(()).expect("Could not send terminate message to server thread");
 
     let join_result = server_thread.unwrap().join();
     if join_result.is_err() {
@@ -263,5 +273,5 @@ fn main() {
         return;
     }
 
-    println!("Terminating!");
+    println!("Closing...");
 }
