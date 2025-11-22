b084338309f2f7769843db4b87a004fe385774e3
Arjan Janssen <arjan.janssen@gmail.com>
2025-11-22 09:46:17 +0100

    heap operation builder test is not an inner class anymore

--- client/src/test/kotlin/com/janssen/memdb/HeapOperationTest.kt
+++ client/src/test/kotlin/com/janssen/memdb/HeapOperationTest.kt
@@ -5,65 +5,65 @@ import org.junit.jupiter.api.Test
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-class HeapOperationTest {
-    class BuilderTest {
-        @Test
-        fun builderCreatesValidAlloc() {
-            val alloc =
-                HeapOperation(
-                    10,
-                    HeapOperationKind.Alloc,
-                    durationSinceServerStart = 200.toDuration(DurationUnit.MILLISECONDS),
-                    address = 2,
-                    size = 4,
-                    threadId = 5,
-                    backtrace = "alloc backtrace",
-                )
-            val builderAlloc =
-                HeapOperation
-                    .Builder(alloc.seqNo)
-                    .alloc(alloc.address, alloc.size)
-                    .threadId(alloc.threadId)
-                    .sinceServerStart(alloc.durationSinceServerStart)
-                    .backtrace(alloc.backtrace)
-                    .build()
-            assertEquals(alloc, builderAlloc)
-        }
-
-        @Test
-        fun builderCreatesValidDealloc() {
-            val seqNo = 10
-            val dealloc =
-                HeapOperation(
-                    10,
-                    HeapOperationKind.Dealloc,
-                    durationSinceServerStart = 500.toDuration(DurationUnit.MILLISECONDS),
-                    address = 3,
-                    size = 0,
-                    threadId = 6,
-                    backtrace = "dealloc backtrace",
-                )
-            val builderDealloc =
-                HeapOperation
-                    .Builder(seqNo)
-                    .dealloc(dealloc.address)
-                    .threadId(dealloc.threadId)
-                    .sinceServerStart(dealloc.durationSinceServerStart)
-                    .backtrace(dealloc.backtrace)
-                    .build()
-            assertEquals(dealloc, builderDealloc)
-        }
-
-        @Test
-        fun builderIncrementsSeqNo() {
-            val seqNo = 10
-            val builder = HeapOperation.Builder(seqNo)
-            assertEquals(seqNo, builder.build().seqNo)
-            assertEquals(seqNo + 1, builder.build().seqNo)
-            assertEquals(seqNo + 2, builder.build().seqNo)
-        }
+class HeapOperationBuilderTest {
+    @Test
+    fun builderCreatesValidAlloc() {
+        val alloc =
+            HeapOperation(
+                10,
+                HeapOperationKind.Alloc,
+                durationSinceServerStart = 200.toDuration(DurationUnit.MILLISECONDS),
+                address = 2,
+                size = 4,
+                threadId = 5,
+                backtrace = "alloc backtrace",
+            )
+        val builderAlloc =
+            HeapOperation
+                .Builder(alloc.seqNo)
+                .alloc(alloc.address, alloc.size)
+                .threadId(alloc.threadId)
+                .sinceServerStart(alloc.durationSinceServerStart)
+                .backtrace(alloc.backtrace)
+                .build()
+        assertEquals(alloc, builderAlloc)
     }
 
+    @Test
+    fun builderCreatesValidDealloc() {
+        val seqNo = 10
+        val dealloc =
+            HeapOperation(
+                10,
+                HeapOperationKind.Dealloc,
+                durationSinceServerStart = 500.toDuration(DurationUnit.MILLISECONDS),
+                address = 3,
+                size = 0,
+                threadId = 6,
+                backtrace = "dealloc backtrace",
+            )
+        val builderDealloc =
+            HeapOperation
+                .Builder(seqNo)
+                .dealloc(dealloc.address)
+                .threadId(dealloc.threadId)
+                .sinceServerStart(dealloc.durationSinceServerStart)
+                .backtrace(dealloc.backtrace)
+                .build()
+        assertEquals(dealloc, builderDealloc)
+    }
+
+    @Test
+    fun builderIncrementsSeqNo() {
+        val seqNo = 10
+        val builder = HeapOperation.Builder(seqNo)
+        assertEquals(seqNo, builder.build().seqNo)
+        assertEquals(seqNo + 1, builder.build().seqNo)
+        assertEquals(seqNo + 2, builder.build().seqNo)
+    }
+}
+
+class HeapOperationTest {
     @Test
     fun protobufAllocRoundTripCommutes() {
         val seqNo = 10
