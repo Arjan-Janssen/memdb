2054a65c69174972b0e3d17b93945af27766970a
Arjan Janssen <arjan.janssen@gmail.com>
2025-11-25 17:05:31 +0100

    added indexed_markers example

---
+++ server/examples/indexed_markers.rs
@@ -0,0 +1,31 @@
+use std::process;
+
+const NUM_ITERATIONS: i64 = 100;
+const GROW_SIZE: i64 = 10000;
+
+fn growing_vec(iteration: i64) {
+    let mut growing_vec = vec![1, 2, 3];
+    for i in 0..GROW_SIZE {
+        growing_vec.push(i);
+    }
+    memdb_lib::server::send_marker_indexed("growing", iteration);
+}
+
+fn main() {
+    let server_thread = memdb_lib::server::run_with_default_address().unwrap_or_else(|error| {
+        println!("Unable to run server on default address: {error:?}");
+        process::exit(-1);
+    });
+    memdb_lib::server::send_marker("begin");
+
+    for i in 0..NUM_ITERATIONS {
+        growing_vec(i);
+        memdb_lib::server::send_marker_indexed("iteration", i);
+    }
+    memdb_lib::server::send_marker("end");
+    memdb_lib::server::send_terminate();
+    server_thread.join().unwrap_or_else(|error| {
+        println!("Unable to join server thread: {error:?}");
+        process::exit(-1);
+    });
+}
